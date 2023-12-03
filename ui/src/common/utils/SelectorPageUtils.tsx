1a535e48df7c78aaa5e72a05eda1d3bc5b42b2cf
saima <saima.sayed@contentstack.com>
2023-12-03 20:07:02 +0530

    resolved bynder example issue and updated node and npm to latest LTS version

--- ui/src/common/utils/SelectorPageUtils.tsx
+++ ui/src/common/utils/SelectorPageUtils.tsx
@@ -1,27 +1,34 @@
 // function to load script
 const loadDAMScript = (url: string) =>
   new Promise((resolve) => {
-    const DAMScript: any = document.createElement("script");
-    const bodyTag = document.getElementsByTagName("body")?.[0];
-    DAMScript.src = url;
-    DAMScript.id = "DAMScript";
-    DAMScript.type = "text/javascript";
-    if (DAMScript?.readyState) {
-      DAMScript.onreadystatechange = function () {
-        if (
-          DAMScript.readyState === "loaded" ||
-          DAMScript.readyState === "complete"
-        ) {
-          DAMScript.onreadystatechange = null;
-          resolve(true);
-        }
-      };
-    } else {
-      DAMScript.onload = function () {
+    const scriptExists = document.getElementById("DAMScript");
+    if (scriptExists) {
+      scriptExists.onload = function () {
         resolve(true);
       };
+    } else {
+      const DAMScript: any = document.createElement("script");
+      const bodyTag = document.getElementsByTagName("body")?.[0];
+      DAMScript.src = url;
+      DAMScript.id = "DAMScript";
+      DAMScript.type = "text/javascript";
+      if (DAMScript?.readyState) {
+        DAMScript.onreadystatechange = function () {
+          if (
+            DAMScript.readyState === "loaded" ||
+            DAMScript.readyState === "complete"
+          ) {
+            DAMScript.onreadystatechange = null;
+            resolve(true);
+          }
+        };
+      } else {
+        DAMScript.onload = function () {
+          resolve(true);
+        };
+      }
+      bodyTag.appendChild(DAMScript);
     }
-    bodyTag.appendChild(DAMScript);
   });
 
 const SelectorPageUtils = {
