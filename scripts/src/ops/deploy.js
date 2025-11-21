9ab6913ecac2d28f8b8f3e72c6df627f5502d3c0
Shradha Nahar <shrdhanhr28@gmail.com>
2025-11-21 15:01:10 +0530

    restructured and refacotored the directory strcuture and files

---
+++ scripts/src/ops/deploy.js
@@ -0,0 +1,117 @@
+const readlineSync = require("readline-sync");
+const {
+  getBaseUrl,
+  getUploadMetaData,
+  getAppBaseUrl,
+  buildAppZip,
+  uploadAppZip,
+  createProject,
+  getProjectDetails,
+  updateLaunchManifest,
+  updateAppManifest,
+  getLaunchManifest,
+  reDeployProject,
+} = require("../utils");
+const loginData = require("../../settings/credentials.json");
+const prodAppManifest = require("../../settings/prod-app-manifest.json");
+
+(async () => {
+  try {
+    const authtoken = loginData?.authtoken;
+    const userOrgs = loginData?.userOrgs;
+    const region = loginData?.region;
+    const appBaseUrl = getAppBaseUrl(region);
+
+    const launchManifest = getLaunchManifest();
+
+    if (!authtoken) {
+      console.info("Login credentials not found. Please login.");
+      return;
+    }
+
+    if (!userOrgs.length) {
+      console.info("No organisations found...");
+      return;
+    }
+
+    orgIndex = readlineSync.keyInSelect(
+      userOrgs.map((org) => org.name),
+      "Please select an organization"
+    );
+    if (orgIndex === -1) {
+      console.info("No organization selected...");
+      return;
+    }
+
+    const selectedOrgUid = userOrgs[orgIndex].uid;
+
+    const projectName = readlineSync.question("Enter the project name: ");
+    const envName = readlineSync.question("Enter the environment name: ");
+    const launchSubDomain = projectName.replace(/ /g, "-");
+
+    const buildPath = buildAppZip();
+
+    const uploadMetaData = await getUploadMetaData(
+      authtoken,
+      appBaseUrl,
+      selectedOrgUid
+    );
+
+    await uploadAppZip(uploadMetaData, buildPath);
+
+    if (launchManifest.created) {
+      console.info("Launch deployment details found, redeploying the app now.");
+
+      const deploymentId = await reDeployProject(
+        authtoken,
+        selectedOrgUid,
+        appBaseUrl,
+        uploadMetaData?.uploadUid,
+        launchManifest.data
+      );
+
+      updateLaunchManifest({
+        ...launchManifest.data,
+        deployment_uid: deploymentId,
+      });
+    } else {
+      const launchMetaData = await createProject(
+        authtoken,
+        selectedOrgUid,
+        appBaseUrl,
+        projectName,
+        uploadMetaData?.uploadUid,
+        envName
+      );
+
+      const launchProjectDetails = await getProjectDetails(
+        appBaseUrl,
+        launchMetaData,
+        authtoken,
+        selectedOrgUid
+      );
+
+      updateLaunchManifest({
+        project_name: projectName,
+        env_name: envName,
+        subdomain: launchSubDomain,
+        ...launchProjectDetails,
+      });
+
+      prodAppManifest.ui_location.base_url =
+        launchProjectDetails?.deployment_url;
+      prodAppManifest.hosting = {
+        provider: "launch",
+        deployment_url: launchProjectDetails?.deployment_url || "",
+        environment_uid: launchProjectDetails?.env_uid || "",
+        project_uid: launchProjectDetails?.project_uid || "",
+      };
+
+      updateAppManifest(prodAppManifest, "prod");
+    }
+  } catch (error) {
+    console.error("Deployment failed:");
+    console.info(error);
+    console.error("Error:", error.message || error);
+  }
+})();
