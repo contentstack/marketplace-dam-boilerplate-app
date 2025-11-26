b83271d57fc811e5a29c1326a2619b82fef0afc9
Shradha Nahar <shrdhanhr28@gmail.com>
2025-11-26 11:45:04 +0530

    refactored create-app script

--- scripts/src/ops/create-update.js
+++ scripts/src/ops/create-update.js
@@ -44,12 +44,65 @@ const devAppManifest = require("../../settings/dev-app-manifest.json");
     }
 
     const selectedOrgUid = userOrgs[orgIndex].uid;
+
+    const createAndDeployApp = async (appManifest) => {
+      const appName = readlineSync.question("Enter name of app: ");
+      appManifest.name = appName;
+
+      const [appError, appData] = await safePromise(
+        createApp(
+          region,
+          authtoken,
+          selectedOrgUid,
+          appName,
+          appManifest.description
+        ),
+        "Error while creating the app."
+      );
+
+      if (appError) {
+        console.error(JSON.stringify(appError, null, 2));
+        return;
+      }
+
+      appManifest.uid = appData;
+
+      const [appUpdateError, appUpdateData] = await safePromise(
+        updateApp(
+          appManifest,
+          region,
+          authtoken,
+          selectedOrgUid,
+          appManifest.uid
+        ),
+        "Error while updating the app."
+      );
+
+      if (appUpdateError) {
+        console.error(JSON.stringify(appUpdateError, null, 2));
+        return;
+      }
+
+      appManifest.ui_location = appUpdateData?.data?.ui_location;
+      updateAppManifest(appManifest, appEnv);
+
+      const url = `${appBaseUrl}/#!/developerhub/app/${appManifest.uid}/ui-locations`;
+      console.info("App created successfully");
+      openLink(url);
+
+      await installApp(
+        appEnv,
+        region,
+        appManifest.uid,
+        csBaseUrl,
+        appBaseUrl,
+        authtoken,
+        selectedOrgUid
+      );
+    };
+
     if (op === "create-app") {
       if (appEnv === "dev") {
-        const appManifest = { ...devAppManifest };
-
-        const appName = readlineSync.question("Enter name of app: ");
-        
         // Always create both field types by default
         console.info("\n⚠️  Note: Both RTE and Custom DAM fields will be created.");
         console.info("   For local development, only one can work at a time:");
@@ -57,147 +110,13 @@ const devAppManifest = require("../../settings/dev-app-manifest.json");
         console.info("   - Custom DAM Field: http://localhost:4000");
         console.info("   Start the appropriate server to test each field type.\n");
 
-        appManifest.hosting = {
-          provider: "external",
-          deployment_url: "http://localhost:4000/#",
-        };
-        appManifest.ui_location = {
-          base_url: "http://localhost:4000/#",
-          locations: [
-            {
-              type: "cs.cm.stack.config",
-              meta: [
-                {
-                  path: "/config",
-                  signed: false,
-                  enabled: true,
-                  required: true,
-                },
-              ],
-            },
-            {
-              type: "cs.cm.stack.custom_field",
-              meta: [
-                {
-                  multiple: false,
-                  path: "/custom-field",
-                  signed: false,
-                  enabled: true,
-                  data_type: "json",
-                  required: false,
-                  name: "DAM DevField",
-                },
-              ],
-            },
-            {
-              type: "cs.cm.stack.rte",
-              meta: [
-                {
-                  name: "RTE DevField",
-                  path: "/dam.js",
-                  signed: false,
-                  enabled: true,
-                  required: false,
-                },
-              ],
-            },
-          ],
-        };
-
-        const [appError, appData] = await safePromise(
-          createApp(
-            region,
-            authtoken,
-            selectedOrgUid,
-            appName,
-            appManifest.description
-          ),
-          "Error while creating the app."
-        );
-
-        appManifest.uid = appData;
-        appManifest.name = appName;
-
-        const [appUpdateError, appUpdateData] = await safePromise(
-          updateApp(
-            appManifest,
-            region,
-            authtoken,
-            selectedOrgUid,
-            appManifest.uid
-          ),
-          "Error while creating the app."
-        );
-        appManifest.ui_location = appUpdateData?.data?.ui_location;
-        updateAppManifest(appManifest, appEnv);
-
-        if (appUpdateError) {
-          console.error(JSON.stringify(appError, null, 2));
-          return;
-        }
-        const url = `${appBaseUrl}/#!/developerhub/app/${appManifest.uid}/ui-locations`;
-
-        console.info("App created successfully");
-        openLink(url);
-
-        await installApp(
-          appEnv,
-          region,
-          appManifest.uid,
-          csBaseUrl,
-          appBaseUrl,
-          authtoken,
-          selectedOrgUid
-        );
+        const appManifest = { ...devAppManifest };
+        await createAndDeployApp(appManifest);
       } else {
-        const appName = readlineSync.question("Enter name of app: ");
-        const appManifest = { ...prodAppManifest, name: appName };
+        const appManifest = { ...prodAppManifest };
 
         if (appManifest.hosting.environment_uid) {
-          const [appError, appData] = await safePromise(
-            createApp(
-              region,
-              authtoken,
-              selectedOrgUid,
-              appName,
-              appManifest.description
-            ),
-            "Error while creating the app."
-          );
-          appManifest.uid = appData;
-          appManifest.name = appName;
-          const [appUpdateError, appUpdateData] = await safePromise(
-            updateApp(
-              appManifest,
-              region,
-              authtoken,
-              selectedOrgUid,
-              appManifest.uid
-            ),
-            "Error while updating the app."
-          );
-          appManifest.ui_location = appUpdateData?.data?.ui_location;
-          updateAppManifest(appManifest, appEnv);
-
-          if (appUpdateError) {
-            console.error(JSON.stringify(appError, null, 2));
-            return;
-          }
-          const url = `${appBaseUrl}/#!/developerhub/app/${appManifest.uid}/ui-locations`;
-
-          console.info("App created successfully");
-          openLink(url);
-
-          await installApp(
-            appEnv,
-            region,
-            appManifest.uid,
-            csBaseUrl,
-            appBaseUrl,
-            authtoken,
-            selectedOrgUid,
-            "BOTH"
-          );
+          await createAndDeployApp(appManifest);
         } else {
           console.info("Launch project is not yet linked");
         }
