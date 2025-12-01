f63b38ee5d44bea148f2b57362e537aadb88a11c
Shradha Nahar <shrdhanhr28@gmail.com>
2025-12-01 18:53:38 +0530

    added dev setup files

--- scripts/src/dev-setup.sh
+++ scripts/src/dev-setup.sh
@@ -7,7 +7,7 @@ npm i
 echo "creating ui/.env file."
 rm -f "./.env"
 cat <<EOF > ".env"
-REACT_APP_UI_URL=http://localhost:4000
+REACT_APP_CUSTOM_FIELD_URL=http://localhost:4000
 REACT_APP_REGION_MAPPING = {"NA": {"JSON_RTE_URL": "https://rte-extension.contentstack.com"},"EU": {"JSON_RTE_URL": "https://eu-rte-extension.contentstack.com"},"AZURE_NA": {"JSON_RTE_URL": "https://azure-na-rte-extension.contentstack.com"},"AZURE_EU": {"JSON_RTE_URL": "https://azure-eu-rte-extension.contentstack.com"}, "GCP_NA": {"JSON_RTE_URL": "https://gcp-na-rte-extension.contentstack.com" }}
 EOF
 
