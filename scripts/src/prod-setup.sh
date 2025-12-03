ee9e12231025c8b32816895bc40c9139aa3ca41a
Shradha Nahar <shrdhanhr28@gmail.com>
2025-12-03 15:53:16 +0530

    script updated for content model creation in setup

--- scripts/src/prod-setup.sh
+++ scripts/src/prod-setup.sh
@@ -6,4 +6,5 @@ npm run deploy-prod-app
 echo "Creating a new production marketplace app."
 npm run create-prod-app
 
-echo "Please run 'npm run create-content-model' to create the content model and its entry"
+echo "Creating content model and its entry for the latest app."
+npm run create-content-model -- latest
